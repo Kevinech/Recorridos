4655173bc959b2c180ace97afd0a77a4f8908e2d
Kevinech <kevinemch@uamv.edu.ni>
2025-11-23 23:13:57 -0600

    Se hizo el Diseño del quinto ejercicio y se termino de hacer la funcionalidad de agregar numeros

---
+++ PracticaRecorridoB/forms/FrmMaxMin.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PracticaRecorridoB.forms
+{
+    partial class FrmMaxMin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbResultado = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNumeros = new System.Windows.Forms.TextBox();
+            this.btnBuscarMaxMin = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBuscarMaxMin);
+            this.groupBox1.Controls.Add(this.tbNumeros);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rtbResultado);
+            this.groupBox1.Location = new System.Drawing.Point(42, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(603, 480);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // rtbResultado
+            // 
+            this.rtbResultado.Location = new System.Drawing.Point(37, 171);
+            this.rtbResultado.Name = "rtbResultado";
+            this.rtbResultado.Size = new System.Drawing.Size(529, 262);
+            this.rtbResultado.TabIndex = 0;
+            this.rtbResultado.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese una lista de numeros separada por coma";
+            // 
+            // tbNumeros
+            // 
+            this.tbNumeros.Location = new System.Drawing.Point(37, 139);
+            this.tbNumeros.Name = "tbNumeros";
+            this.tbNumeros.Size = new System.Drawing.Size(447, 26);
+            this.tbNumeros.TabIndex = 2;
+            // 
+            // btnBuscarMaxMin
+            // 
+            this.btnBuscarMaxMin.Location = new System.Drawing.Point(491, 135);
+            this.btnBuscarMaxMin.Name = "btnBuscarMaxMin";
+            this.btnBuscarMaxMin.Size = new System.Drawing.Size(75, 30);
+            this.btnBuscarMaxMin.TabIndex = 3;
+            this.btnBuscarMaxMin.Text = "buscar";
+            this.btnBuscarMaxMin.UseVisualStyleBackColor = true;
+            this.btnBuscarMaxMin.Click += new System.EventHandler(this.btnBuscarMaxMin_Click);
+            // 
+            // FrmMaxMin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMaxMin";
+            this.Text = "FrmMaxMin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBuscarMaxMin;
+        private System.Windows.Forms.TextBox tbNumeros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbResultado;
+    }
+}
---
+++ PracticaRecorridoB/forms/FrmMaxMin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaRecorridoB.forms
+{
+    public partial class FrmMaxMin : Form
+    {
+        public FrmMaxMin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarMaxMin_Click(object sender, EventArgs e)
+        {
+            // Leer la cadena de números del TextBox
+            string entrada = tbNumeros.Text;
+            if (string.IsNullOrWhiteSpace(entrada))
+            {
+                MessageBox.Show("Ingrese una lista de números separados por coma.", "Error");
+                return;
+            }
+
+            // Convertir la cadena en lista de enteros
+            string[] partes = entrada.Split(',');
+            List<int> listaNumeros = new List<int>();
+
+            foreach (string strNum in partes)
+            {
+                int num;
+                if (int.TryParse(strNum.Trim(), out num))
+                {
+                    listaNumeros.Add(num);
+                }
+            }
+
+            if (listaNumeros.Count == 0)
+            {
+                MessageBox.Show("No se ingresaron números válidos.", "Error");
+                return;
+            }
+
+            // Buscar máximo, mínimo e iteraciones
+            int maximo, minimo, ciclos;
+            BuscarMaxMin(listaNumeros, out maximo, out minimo, out ciclos);
+
+            // Presentar el resultado en el RichTextBox
+            rtbResultado.Clear();
+            rtbResultado.AppendText($"Máximo: {maximo}\n");
+            rtbResultado.AppendText($"Mínimo: {minimo}\n");
+            rtbResultado.AppendText($"Iteraciones realizadas: {ciclos}\n");
+
+        }
+        private void BuscarMaxMin(List<int> listaEnteros, out int max, out int min, out int iteraciones)
+        {
+            if (listaEnteros == null || listaEnteros.Count == 0)
+            {
+                throw new ArgumentException("La lista no puede estar vacía.");
+            }
+
+            max = listaEnteros[0];
+            min = listaEnteros[0];
+            iteraciones = 0;
+
+            foreach (int valor in listaEnteros)
+            {
+                iteraciones++;
+                if (valor > max)
+                    max = valor;
+                if (valor < min)
+                    min = valor;
+            }
+        }
+
+    }
+}
